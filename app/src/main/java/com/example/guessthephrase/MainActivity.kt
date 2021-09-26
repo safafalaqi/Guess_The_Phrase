9578f924f8195530c6bc1544f19e6d3e1761a8b7
Safa <sfalaqi@gmail.com>
2021-09-27 00:17:02 +0300

    initial commit

--- app/src/main/java/com/example/guessthephrase/MainActivity.kt
+++ app/src/main/java/com/example/guessthephrase/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,64 +14,98 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    var guess = 3
+    private lateinit var textEdit: EditText
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var myRv: RecyclerView
+
+    private var guessPhraseCount = 10
+    private var guessLetterCount = 10
+    private val str ="Coding Dojo is great"
+    private var guessedPhrase = arrayListOf<String>()
+    private var guessedLetters = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //generate a random number from 0 to 10
-        var random = Random.nextInt(11)
-        val texts = arrayListOf<String>()
-        val button = findViewById<Button>(R.id.button)
-        val editText = findViewById<EditText>(R.id.enterNumber)
-        val myRv = findViewById<RecyclerView>(R.id.rvMain)
+        textView =findViewById<TextView>(R.id.textView)
+        textEdit= findViewById<EditText>(R.id.enteredGuess)
+        button = findViewById<Button>(R.id.button)
+        myRv = findViewById<RecyclerView>(R.id.rvMain)
 
 
-        button.setOnClickListener {
-            //first check if empty and if yes inform the user
-            if (editText.text.isEmpty()) {
-                Toast.makeText(this, "Please enter a number ", Toast.LENGTH_SHORT).show()
-                editText.getText().clear()
-            } else if(guess==0)  //if we have 0 guess
-            {
-                //show the user a dialog if want to play again or not
-                showAlertDialog()
-                editText.getText().clear()
-            } //if user give a correct guess print in recycler view that it is correct
-            else if(random == Integer.parseInt(editText.text.toString())) {
-                texts.add("You guessed ${editText.text.toString()}\n Correct Guess! You win.")
-                //then clear the Edit Text field.
-                editText.getText().clear()
-                myRv.adapter = RecyclerViewAdapter(texts)
-                myRv.layoutManager = LinearLayoutManager(this)
-                //set guess to 0 so the dialog box appear and the user will choose to play or not
-                guess=0
-                showAlertDialog()
-            } else {
-                texts.add("You guessed ${editText.text.toString()}\n You have $guess guesses left")
-                //then clear the Edit Text field.
-                editText.getText().clear()
-                myRv.adapter = RecyclerViewAdapter(texts)
-                myRv.layoutManager = LinearLayoutManager(this)
-                guess--
-            }
-
+        //take a string and convert each letter into a star character
+        //replace all characters to *
+        textView.text="phrase: ${str.replace("[^\\s]".toRegex(), "*")}"
+        //- ask the user to guess a predefined phrase
+        button.setOnClickListener{
+         // if(guessPhraseCount>0)
+            guessPhrase()
+            //else
+            //guessLetter()
         }
+       //  - ask the user to guess a letter from the phrase if they cannot guess the full phrase
+       // - check the phrase for the guessed letter and convert stars into correctly guessed letters
+       // - track guessed letters and display them to the user
+        //- allow the user to guess the full phrase 10 times, the user should be able to enter a phrase during this stage
+        //- allow the user to guess 10 letters, the user should only be able to enter single letters during this stage
+       // - change the Entry Text hint to reflect whether the user is guessing the phrase or a letter
+
+
     }
 
-    //this function is used to show Alert Dialog for the user
-    fun showAlertDialog()
-    {
+    private fun guessLetter() {
 
+    }
+
+    private fun guessPhrase(){
+        val guess = textEdit.text.toString()
+
+        if (textEdit.text.isEmpty()) {
+            Toast.makeText(this, "Please enter a phrase ", Toast.LENGTH_SHORT).show()
+            textEdit.getText().clear()
+        } else if(guessPhraseCount==0)  //if we have 0 guess
+        {
+            showAlertDialog("You Lost")
+            textEdit.getText().clear()
+        }
+        else if(guess==str) {
+            showAlertDialog("You win")
+            textEdit.text.clear()
+            guessedPhrase.add("You guessed ${textEdit.text.toString()}\n Correct Guess! You win.")
+            //then clear the Edit Text field.
+            textEdit.getText().clear()
+            myRv.adapter = RecyclerViewAdapter(guessedPhrase)
+            myRv.layoutManager = LinearLayoutManager(this)
+            //set guess to 0 so the dialog box appear and the user will choose to play or not
+           }
+           else
+            {
+                guessedPhrase.add("Wrong guess: ${textEdit.text.toString()}")
+                guessedPhrase.add("$guessPhraseCount guesses reamining")
+
+                //then clear the Edit Text field.
+                textEdit.getText().clear()
+                myRv.adapter = RecyclerViewAdapter(guessedPhrase)
+                myRv.layoutManager = LinearLayoutManager(this)
+                guessPhraseCount--
+            }
+        myRv.scrollToPosition(guessedPhrase.size - 1)
+    }
+
+
+
+    //this function is used to show Alert Dialog for the user
+    private fun showAlertDialog(str:String)
+    {
         // first we create a variable to hold an AlertDialog builder
         val dialogBuilder = AlertDialog.Builder(this)
         // here we set the message of our alert dialog
-        dialogBuilder.setMessage("Would you like to play again?")
+        dialogBuilder.setMessage("$str\nWould you like to play again?")
             // positive button text and action
             .setPositiveButton("Play", DialogInterface.OnClickListener {
                     dialog, id ->  this.recreate()
-                guess=3
+
             })
             // negative button text and action
             .setNegativeButton("Stop", DialogInterface.OnClickListener {
@@ -79,10 +114,9 @@ class MainActivity : AppCompatActivity() {
         // create dialog box
         val alert = dialogBuilder.create()
         // set title for alert dialog box
-        alert.setTitle("New Message")
+        alert.setTitle("Game Over")
         // show alert dialog
         alert.show()
-
     }
 
 }
